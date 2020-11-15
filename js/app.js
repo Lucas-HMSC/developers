5a7444c23ebe71d6cdfa00e1e2fa649ef9c29371
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-11-15 16:03:56 -0300

    Slide Otimizado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+/*
+* Author: Lucas-HMSC    
+* Curso: JavaScript com jQuery (Origamid)
+*/
+// Mudar TAB ao CLICK
 $('[data-group]').each(function(){
     let $allTarget = $(this).find('[data-target]'),
         $allClick = $(this).find('[data-click]'),
@@ -20,6 +25,7 @@ $('[data-group]').each(function(){
     });
 });
 
+// Scroll suave para link interno
 $('.menu-nav a[href^="#"]').click(function(e){
     e.preventDefault();
     let id = $(this).attr('href'),
@@ -31,6 +37,7 @@ $('.menu-nav a[href^="#"]').click(function(e){
     }, 500)
 });
 
+// Scroll suave para o topo
 $('.logo').click(function(e){
     e.preventDefault();
     $('html, body').animate({
@@ -38,6 +45,7 @@ $('.logo').click(function(e){
     }, 500)
 });
 
+// Muda o menu para active de acordo com a área
 $('section').each(function(){
     let height = $(this).height(),
         offsetTop = $(this).offset().top,
@@ -56,23 +64,37 @@ $('section').each(function(){
     });
 });
 
+// Botão do menu mobile
 $('.mobile-btn').click(function(){
     $(this).toggleClass('active');
     $('.mobile-menu').toggleClass('active');
 });
 
-$('.slide > :first').addClass('active');
+// Slider
+function slider (sliderName, velocidade) {
+    let sliderClass = '.' + sliderName,
+        activeClass = 'active',
+        rotate = setInterval(rotateSlide, velocidade);
 
-function rotateSlide() {
-    let activeSlide = $('.slide > .active'),
-        nextSlide = activeSlide.next();
-    
-    if (nextSlide.length == 0) {
-        nextSlide = $('.slide > :first');
+    $(sliderClass + ' > :first').addClass(activeClass);
+
+    $(sliderClass).hover(function(){
+        clearInterval(rotate);
+    }, function(){
+        rotate = setInterval(rotateSlide, velocidade);
+    });
+
+    function rotateSlide() {
+        let activeSlide = $(sliderClass + ' > .' + activeClass),
+            nextSlide = activeSlide.next();
+        
+        if (nextSlide.length == 0) {
+            nextSlide = $(sliderClass + ' > :first');
+        }
+
+        activeSlide.removeClass(activeClass);
+        nextSlide.addClass(activeClass);
     }
-
-    activeSlide.removeClass('active');
-    nextSlide.addClass('active');
 }
 
-setInterval(rotateSlide, 2000);
+slider('introducao', 2000);
