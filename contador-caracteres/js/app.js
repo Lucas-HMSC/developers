9e21b37896779b8119987d3d7616af3334cda1d8
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2020-11-16 15:07:32 -0300

    Update app.js

--- contador-caracteres/js/app.js
+++ contador-caracteres/js/app.js
@@ -7,18 +7,29 @@ $('textarea').on('keyup paste', function(){
     }, 200);
 });
 
-$('.cxAlta').click(function cxAlta(){
+$('.cxAlta').click(function(){
     let textoAntes = $('textarea').val(),
         textoDepois = textoAntes.toUpperCase();
     document.querySelector('textarea').value = textoDepois;
 });
 
-$('.cxBaixa').click(function cxAlta(){
+$('.cxBaixa').click(function(){
     let textoAntes = $('textarea').val(),
         textoDepois = textoAntes.toLowerCase();
     document.querySelector('textarea').value = textoDepois;
 });
 
-
-
+$('.capitalizar').click(function(){
+    let textoAntes = $('textarea').val().split(' ');
+    let textoDepois = '';
+    for (i in textoAntes) {
+        let input = textoAntes[i],
+            lower = input.toLowerCase(),
+            pLetra = lower.slice(0, 1),
+            cap = lower.replace(pLetra, pLetra.toUpperCase());
+        textoDepois += cap + ' ';
+    }
+    document.querySelector('textarea').value = textoDepois;
+    //console.log(textoDepois);
+});
 
